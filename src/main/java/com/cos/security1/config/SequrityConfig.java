fd0c60aa224d9ba1e41c9926510d98e265a85b91
ChaKyungSuk <cha0054@naver.com>
2023-07-03 22:58:55 +0900

    first security commit

---
+++ src/main/java/com/cos/security1/config/SequrityConfig.java
@@ -0,0 +1,22 @@
+package com.cos.security1.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 됩니다.
+public class SequrityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.authorizeRequests()
+			.antMatchers("/user/**").authenticated() // 이주소면 인증이 필요해
+			.antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+			.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+			.anyRequest().permitAll();
+	}
+	
+}
